4a247ac491ee507f6c1a388ca941f656ff14229a
matheussacg <matheus.sacg@gmail.com>
2026-08-17 09:15:32 -0300

    feat: implementa sessão em memória com refresh e logout

--- src/services/autenticacao/types.ts
+++ src/services/autenticacao/types.ts
@@ -1,6 +1,9 @@
-export type SessaoAutenticacao = {
-  token: string
+export type PerfilUsuario = {
   rf: string
   nome: string
   descricaoCargo: string
+}
+
+export type SessaoAutenticacao = PerfilUsuario & {
+  token: string
 }
